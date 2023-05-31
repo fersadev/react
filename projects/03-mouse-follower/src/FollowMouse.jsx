eab957d214955245cf862a175da39491a98534cd
jorgesabapps <jorgesabapps@outlook.com>
2023-05-31 12:41:30 -0500

    Creado el proyecto 3 utilizando useEffects

---
+++ projects/03-mouse-follower/src/FollowMouse.jsx
@@ -0,0 +1,22 @@
+export function FollowMouse(position){
+    return (
+        <>
+            <div style={{
+            position: 'absolute',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            border: '1px solid #fff',
+            borderRadius: '50%',
+            opacity: 0.8,
+            pointerEvents: 'none',
+            left: -25,
+            top: -25,
+            width: 50,
+            height: 50,
+            transform: `translate(${position.x}px, ${position.y}px)`
+        }}>
+        </div>
+        <h3>Proyecto 3</h3>
+        
+        </>
+    )
+}
